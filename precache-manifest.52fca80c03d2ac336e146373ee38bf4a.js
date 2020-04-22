4669744d0b3fe8974caebb8e0642d9c5b2572ce8
guychien <guychienll@gmail.com>
2020-04-22 08:03:35 +0800

    Updates

---
+++ precache-manifest.52fca80c03d2ac336e146373ee38bf4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04e6f592e59992060424cda95ee726f",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "636c912d17dad10fe0b6",
+    "url": "/tictactoe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bebfcab51c0600e7783d",
+    "url": "/tictactoe/static/js/2.f2dc6e5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tictactoe/static/js/2.f2dc6e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636c912d17dad10fe0b6",
+    "url": "/tictactoe/static/js/main.7d1917a0.chunk.js"
+  },
+  {
+    "revision": "4ce048a5a421bf4ab11e",
+    "url": "/tictactoe/static/js/runtime-main.f58de196.js"
+  }
+]);
